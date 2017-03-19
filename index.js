8bdd16ef42768abd57a9064ba19fd55b7d3e592f
github username <zylberberg.jonathan@gmail.com>
2017-03-19 14:39:10 -0400

    put picture, added answers, and more

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $( document ).ready(function) {
 	$(".button-collapse").sideNav();
 }
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#main').css('opacity') = '0.6'
+    }
+});
